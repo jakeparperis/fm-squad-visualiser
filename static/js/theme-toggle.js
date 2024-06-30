fe1c8892ae32059da5ade53cc6599bab10e694aa
jakep <jakeparperis@gmail.com>
2024-06-30 15:39:17 +0300

    Update theme toggle button

--- static/js/theme-toggle.js
+++ static/js/theme-toggle.js
@@ -1,15 +1,19 @@
 function toggleTheme() {
     const body = document.body;
+    const button = document.getElementById('theme-toggle-button')
     const currentTheme = body.getAttribute('data-theme');
 
     if (currentTheme === 'light') {
         body.setAttribute('data-theme', 'colour1');
+        button.textContent = 'Theme 🔵';
     }
     else if (currentTheme === 'colour1') {
         body.setAttribute('data-theme', 'dark');
+        button.textContent = 'Theme ⚫️';
     }
     else if (currentTheme === 'dark') {
         body.setAttribute('data-theme', 'light')
+        button.textContent = 'Theme ⚪️';
     }
 
     localStorage.setItem('theme', body.getAttribute('data-theme'))
@@ -19,10 +23,18 @@ document.getElementById('theme-toggle-button').addEventListener('click', toggleT
 
 function applyStoredTheme() {
     const body = document.body;
+    const button = document.getElementById('theme-toggle-button');
     const storedTheme = localStorage.getItem('theme');
 
     if (storedTheme) {
         body.setAttribute('data-theme', storedTheme);
+        if (storedTheme === 'light') {
+            button.textContent = 'Theme ⚪️';
+        } else if (storedTheme === 'colour1') {
+            button.textContent = 'Theme 🔵';
+        } else if (storedTheme === 'dark') {
+            button.textContent = 'Theme ⚫️';
+        }
     }
 }
 
