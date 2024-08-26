6d5a0be27297c84562b01d9d58d953053a221ab8
jakep <jakeparperis@gmail.com>
2024-08-26 16:22:33 +0300

    Prototype homepage design

---
+++ static/js/help-button.js
@@ -0,0 +1,4 @@
+document.querySelector(".help-button").addEventListener("click", function() {
+    var helpContent = document.getElementById("helpContent");
+    helpContent.classList.toggle("visible");
+});
